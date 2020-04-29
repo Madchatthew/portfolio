4595e3bf3bcd7250de18495295571380cc6a3c61
Madchatthew <chad.jessen@gmail.com>
2020-04-29 15:25:30 -0400

    WIP

--- js/portfolio.js
+++ js/portfolio.js
@@ -12,8 +12,4 @@ $(document)
         $('.top.menu').transition('fade out');
       }
     });
-
-  // create sidebar and attach to menu open
-  // $('.ui.icon')
-  //   .sidebar('attach events', '.icon.menu');
 });
