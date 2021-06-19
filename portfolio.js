f1d33e55713c27cb8b23942125ee47abc66f4618
Madchatthew <chad.jessen@gmail.com>
2021-06-19 02:34:52 -0500

    wip

--- portfolio.js
+++ portfolio.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.send('This is the index route.');
 });
