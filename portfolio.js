56d33e56d48fab2f42653c87cb0f0ee4dd5ea210
Madchatthew <chad.jessen@gmail.com>
2021-09-22 18:37:25 -0500

    finished coding on contact page

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,4 @@
-// Variables setup
+// Application requirements
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
@@ -6,16 +6,23 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const nodemailer = require('nodemailer');
 const app = express();
 
+// End application requirements
+
 // App setup section
+
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(expressLayouts);
 
 // Routes section
+
 const indexRouter = require('./routes/index');
 const aboutRouter = require('./routes/about');
 const skillsRouter = require('./routes/skills');
