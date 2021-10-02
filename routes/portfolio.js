36ddf9a4c3244568d20a0b566a10af1350778c4e
Madchatthew <chad.jessen@gmail.com>
2021-10-02 11:39:59 -0500

    started github portfolio api integration

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    const url = "https://api.github.com/users/Madchatthew/repos"
+
     res.render('portfolio/index', { layout: './layouts/portfolioLayout' });
 });
 
