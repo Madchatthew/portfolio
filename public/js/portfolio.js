8b5ca85c2978aac309313ef1dfb488868c2c01ba
Madchatthew <chad.jessen@gmail.com>
2021-10-11 22:22:54 -0500

    portfolio layout and cosmetic changes, some backend changes

---
+++ public/js/portfolio.js
@@ -0,0 +1,13 @@
+const homepage = document.querySelector('#homepage');
+const card-content = document.querySelector('.card-content');
+const homepageLink = querySelector('a');
+const href = homepageLink.getAttribute('href');
+
+homepage.forEach(() => {
+    if (href = "") {
+        homepage.classList.remove(card-content);
+    }
+})
+
+
+
