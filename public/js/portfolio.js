070a00199775363b4617160538e0daea557c4683
Madchatthew <chad.jessen@gmail.com>
2021-10-13 01:09:27 -0500

    solved see live site issue, started working on date formatting

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,13 +1,2 @@
-const homepage = document.querySelector('#homepage');
-const card-content = document.querySelector('.card-content');
-const homepageLink = querySelector('a');
-const href = homepageLink.getAttribute('href');
-
-homepage.forEach(() => {
-    if (href = "") {
-        homepage.classList.remove(card-content);
-    }
-})
-
 
 
