c28f3be5cc38b567037b5dcb898b6b2c426e0426
Madchatthew <chad.jessen@gmail.com>
2021-10-29 23:54:27 -0500

    started experience page and styling - gonna keep it nice and simple

--- portfolio.js
+++ portfolio.js
@@ -28,12 +28,14 @@ app.use(expressLayouts);
 
 const indexRouter = require('./routes/index');
 const aboutRouter = require('./routes/about');
+const experienceRouter = require('./routes/experience');
 const portfolioRouter = require('./routes/portfolio');
 const skillsRouter = require('./routes/skills');
 const contactRouter = require('./routes/contact');
 
 app.use('/', indexRouter);
 app.use('/about', aboutRouter);
+app.use('/experience', experienceRouter);
 app.use('/portfolio', portfolioRouter);
 app.use('/skills', skillsRouter);
 app.use('/contact', contactRouter);
